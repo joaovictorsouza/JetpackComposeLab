be22815d9560e36a5611c2533c54dfd5a1f0e6f9
jvsouzavx <jsouza-magit@vortx.com.br>
2022-09-06 15:01:52 -0300

    Initial commit

---
+++ wear/src/main/java/br/eng/joaovictor/assistant/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package br.eng.joaovictor.assistant.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun AssistantTheme(
+        content: @Composable () -> Unit
+) {
+    MaterialTheme(
+            colors = wearColorPalette,
+            typography = Typography,
+            // For shapes, we generally recommend using the default Material Wear shapes which are
+            // optimized for round and non-round devices.
+            content = content
+    )
+}
