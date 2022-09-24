6bfc42a8d7955e753d920aa44ae51e0cfe8f80f8
jvsouzavx <jsouza-magit@vortx.com.br>
2022-09-24 19:03:03 -0300

    feat: implement screen logic, componentization of some items, adjust room and coroutine flows

---
+++ mobile/src/main/java/br/eng/joaovictor/assistant/presentation/add_edit_task/components/AddTaskHeader.kt
@@ -0,0 +1,100 @@
+package br.eng.joaovictor.assistant.presentation.add_edit_task.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import br.eng.joaovictor.assistant.R
+
+@Composable
+fun AddTaskHeader(title : String, description : String, modifier: Modifier = Modifier) {
+    val configuration = LocalConfiguration.current
+    val width = configuration.screenWidthDp
+
+    Row {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                Modifier
+                    .width((width * 1.5).dp)
+                    .offset(y = (-76).dp)
+                    .wrapContentSize(unbounded = true)
+            ) {
+                CreateIcon(true)
+            }
+            Spacer(modifier = modifier.height(16.dp))
+            Text(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                text = title,
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                modifier = Modifier.padding(start = 26.dp, end = 26.dp),
+                text = description,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+
+@Composable
+fun CreateIcon(isLarge: Boolean = false) {
+    val colors =
+        listOf(MaterialTheme.colorScheme.onPrimary, MaterialTheme.colorScheme.primaryContainer)
+    val painter = painterResource(id = R.drawable.ic_add_task)
+    val scaleImg = if (isLarge) 0.99f else .5f
+    val heightImgScale = if (isLarge) 30f else .20f
+    val widthImgScale = if (isLarge) 40f else .6f
+    val drawSize = if (isLarge) 400.dp else 180.dp
+    val imgColor = MaterialTheme.colorScheme.onPrimaryContainer
+
+    Canvas(
+        modifier = Modifier
+            .size(drawSize)
+    ) {
+        rotate(degrees = 45f) {
+            drawOval(
+                size = Size(
+                    width = drawSize.toPx(),
+                    height = drawSize.toPx() * 1.3f - 50.dp.toPx()
+                ),
+                topLeft = Offset(
+                    x = 50.dp.toPx(),
+                    y = 0.dp.toPx()
+                ),
+                brush = Brush.linearGradient(colors = colors)
+
+            )
+        }
+        with(painter) {
+            scale(
+                scale = scaleImg,
+                pivot = Offset(size.width * widthImgScale, size.height * heightImgScale)
+            ) {
+                draw(
+                    size = painter.intrinsicSize,
+                    alpha = 1f,
+                    colorFilter = ColorFilter.tint(imgColor)
+                )
+            }
+        }
+
+    }
+}
---
+++ mobile/src/main/java/br/eng/joaovictor/assistant/presentation/add_edit_task/components/TaskTypeSelection.kt
@@ -0,0 +1,72 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package br.eng.joaovictor.assistant.presentation.add_edit_task.components
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import br.eng.joaovictor.assistant.R
+import br.eng.joaovictor.assistant.data.datasource.enums.TaskTypeEnum
+import br.eng.joaovictor.assistant.presentation.add_edit_task.AddEditTaskEvent
+
+@Composable
+fun TaskTypeSelection(selectedType: TaskTypeEnum, onSelected: (TaskTypeEnum) -> Unit, modifier: Modifier = Modifier) {
+    LazyRow(
+        Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = 30.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        item {
+            InputChip(
+                selected = selectedType == TaskTypeEnum.HABIT,
+                onClick = { onSelected(TaskTypeEnum.HABIT) },
+                label = {
+                    Text(
+                        stringResource(R.string.chip_habit),
+                        color = MaterialTheme.colorScheme.surfaceTint
+                    )
+                },
+                interactionSource = remember { MutableInteractionSource() },
+
+                )
+        }
+        item {
+            InputChip(
+                selected = selectedType == TaskTypeEnum.RECURRENT_TASK,
+                onClick = { onSelected(TaskTypeEnum.RECURRENT_TASK) },
+
+                label = {
+                    Text(
+                        stringResource(R.string.chip_recurrent_task),
+                        color = MaterialTheme.colorScheme.surfaceTint
+                    )
+                },
+                interactionSource = remember { MutableInteractionSource() },
+            )
+        }
+        item {
+            InputChip(
+                selected = selectedType == TaskTypeEnum.TASK,
+                onClick = { onSelected(TaskTypeEnum.TASK) },
+                label = {
+                    Text(
+                        stringResource(R.string.chip_task),
+                        color = MaterialTheme.colorScheme.surfaceTint
+                    )
+                },
+                interactionSource = remember { MutableInteractionSource() },
+            )
+        }
+    }
+}
